faf60d288a1a1ce31922a7765b18ae002365f7e9
xfl <xfl@ncmem.com>
2017-09-20 15:37:24 +0800

    load_complete取消隐藏操作

--- WordPaster/js/WordPaster.js
+++ WordPaster/js/WordPaster.js
@@ -592,7 +592,7 @@ function WordPasterManager()
             }
         }
         if (needUpdate) this.need_update();
-        else { $.skygqbox.hide(); }
+        //else { $.skygqbox.hide(); }
     };
     this.recvMessage = function (msg)
 	{
